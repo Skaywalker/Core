e28329e688a859adc99e2c3e22601e60fe0743c1
Skaywalker <115165402+Skaywalker@users.noreply.github.com>
2024-07-14 22:59:38 +0200

    chach fix

---
+++ bootstrap/cache/main_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Main\\Providers\\MainServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Main\\Providers\\MainServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
